15c03186f42e9796d53cdbdb8c1c8d0f164ca5e7
Andra <andrej.kozlina996@gmail.com>
2025-03-11 13:34:34 +0100

    remove commented code

--- app/Services/Word/RemainingWordsService.php
+++ app/Services/Word/RemainingWordsService.php
@@ -1,47 +1,4 @@
 <?php
-//
-//namespace App\Services\Word;
-//
-//use App\Repositories\Games\GameInterface;
-//use App\Repositories\Submissions\SubmissionInterface;
-//
-//class RemainingWordsService
-//{
-//    public function getRemainingLetters($gameId)
-//    {
-//        $game = app(GameInterface::class)->get($gameId);
-//        $submission = app(SubmissionInterface::class)->getByGameId($gameId);
-//        $usedWords = !is_null($submission) ? $submission->pluck('word') : '';
-//
-//        $availableLetters = str_split($game->letters);
-//        $usedLetters = str_split($usedWords);
-//
-//        foreach ($usedLetters as $letter) {
-//            if (($key = array_search($letter, $availableLetters)) !== false) {
-//                unset($availableLetters[$key]);
-//                $availableLetters = array_values($availableLetters);
-//            }
-//        }
-//
-//        return implode('', $availableLetters);
-//    }
-//
-//    public function getValidWordsFromLetters($letters)
-//    {
-//        $words = file(storage_path('words.txt'), FILE_IGNORE_NEW_LINES);
-//        $valid_words = [];
-//
-//        foreach ($words as $word) {
-//            if ((new WordListService())->canFormWord($word, $letters)) {
-//                $valid_words[] = $word;
-//            }
-//        }
-//
-//        return $valid_words;
-//    }
-//}
-
-
 namespace App\Services\Word;
 
 use App\Repositories\Games\GameInterface;
